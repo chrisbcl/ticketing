9674a5abc6cee326b3ec1e5d57609b52fbbabbb6
Chris <chrisbcl@hotmail.com>
2020-10-22 01:07:36 +0100

    fixed test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -5,7 +5,7 @@ import { signUp } from '../../test/utils';
 it('responds with details about current user', async () => {
     const cookie = await signUp();
 
-    const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(400);
+    const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(200);
 
     expect(response.body.currentUser.email).toEqual('test@test.com');
 });
